18bb323ff2158f43c2f0f7702927cfef69f4a7b0
Dyzzie <181732343+dyzzie0@users.noreply.github.com>
2024-12-14 19:46:44 +0000

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,89 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+    })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
+
+function displayText() {
+  var text = document.getElementById("textField");
+  text.style.display = "block";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+  slides[slideIndex-1].style.display = "block";
+  }
+} 
+
+document.addEventListener('DOMContentLoaded', () => {
+const form = document.getElementById('contactForm');
+  
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+const name = document.getElementById('name').value.trim();
+const email = document.getElementById('email').value.trim();
+const phone = document.getElementById('phone').value.trim();
+const message = document.getElementById('message').value.trim();
+
+let isValid = true;
+if (name === "") 
+  {isValid = false;
+document.getElementById('nameError').textContent = "Please enter your name.";}   
+else{
+document.getElementById('nameError').textContent = ""; }
+if (email === ""){ 
+isValid = false;
+
+document.getElementById('emailError').textContent = "Email is required.";} 
+else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+isValid = false;
+
+document.getElementById('emailError').textContent = "Enter a valid email.";} 
+else {
+document.getElementById('emailError').textContent = "";}
+if (phone === "") {
+isValid = false;
+
+document.getElementById('phoneError').textContent = "Phone number is required.";} 
+else {
+document.getElementById('phoneError').textContent = "";}
+if (message === "")
+   {
+    isValid = false;
+    document.getElementById('messageError').textContent = "Message is required.";} 
+else {
+document.getElementById('messageError').textContent = "";  } if (isValid) {
+window.location.href = "ThankYou.html"; 
+}
+});
+});
+
+
+
+
+
+
+
+
